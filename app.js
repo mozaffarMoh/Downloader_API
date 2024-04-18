232f7747b15e36cdb777ee6697d01ab750f78eae
Mozaffar <FERAWWWESS@gmail.com>
2024-04-18 20:56:26 +0300

    changeTO4000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { pipeline } = require("stream");
 const ytdl = require("ytdl-core");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
